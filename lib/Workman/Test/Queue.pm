766cdead0c3a741373e2680be96c70b5a580366e
karupanerura <karupa@cpan.org>
2014-07-29 21:56:04 +0900

    Revert "skip parallel testing in TRAVIS-CI."
    This reverts commit 9774039afa2964fb00749955c3af26ede6177ef1.

--- lib/Workman/Test/Queue.pm
+++ lib/Workman/Test/Queue.pm
@@ -29,7 +29,7 @@ sub new {
     return bless {
         queue   => $queue,
         taskset => $taskset,
-        verbose => 0,
+        verbose => $ENV{TRAVIS} ? 1 : 0,
         t       => Test::Builder->new,
         json    => JSON::XS->new->ascii->canonical->allow_nonref->allow_blessed,
     } => $class;
@@ -44,7 +44,7 @@ sub plans {
        { name => 'register_tasks', tests => 1 },
        { name => 'dequeue',        tests => 4 },
     );
-    if ($self->queue->can_wait_job && !$self->queue->isa('Workman::Queue::Mock') && !$ENV{TRAVIS}) {
+    if ($self->queue->can_wait_job && !$self->queue->isa('Workman::Queue::Mock')) {
         push @plans => { name => 'parallel', tests => 101 };
     }
     return @plans;
